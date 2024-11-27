8736b0ed57b9dcccf649dfcb675913120defba56
muthamizhyadav <muthamizhyadav@gmail.com>
2024-11-27 22:58:31 +0530

    jobs CHanges

--- src/middlewares/candAuth.js
+++ src/middlewares/candAuth.js
@@ -4,12 +4,11 @@ const config = require('../config/config');
 const { Candidate } = require('../models/candidate.model');
 
 const Auth = async (req, res, next) => {
-  const token = req.headers.auth;
-  console.log(!token, "TOK");
-  
-  if (!token) {
-    return res.send(httpStatus.UNAUTHORIZED, 'user must be LoggedIn....');
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(httpStatus.UNAUTHORIZED).send('User must be logged in.');
   }
+  const token = authHeader.split(' ')[1];
   try {
     const payload = jwt.verify(token, config.jwt.secret);
     console.log(payload, "Payload");
