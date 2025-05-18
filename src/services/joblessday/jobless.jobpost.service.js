dd28e6aab49bae57ec6123b20cc0e5ddce629cef
muthamizhyadav <muthamizhyadav@gmail.com>
2025-05-18 22:17:37 +0530

    Time Change

--- src/services/joblessday/jobless.jobpost.service.js
+++ src/services/joblessday/jobless.jobpost.service.js
@@ -1,24 +1,23 @@
 const httpStatus = require('http-status');
 const ApiError = require('../../utils/ApiError');
 const JoblessJobPost = require('../../models/joblessday/jobless.post');
-const moment = require('moment')
+const moment = require('moment');
 
 const createJobPost = async (req) => {
   const userId = req.userId;
-  const { startTime, endTime } = req.body;
-const istStartTime = moment.utc(startTime).add(5, 'hours').add(30, 'minutes').toDate();
-const istEndTime = moment.utc(endTime).add(5, 'hours').add(30, 'minutes').toDate();
-console.log("IST Start Time:", istStartTime);
-console.log("IST End Time:", istEndTime);
+  //   const { startTime, endTime } = req.body;
+  // const istStartTime = moment.utc(startTime).add(5, 'hours').add(30, 'minutes').toDate();
+  // const istEndTime = moment.utc(endTime).add(5, 'hours').add(30, 'minutes').toDate();
+  // console.log("IST Start Time:", istStartTime);
+  // console.log("IST End Time:", istEndTime);
 
-const creation = await JoblessJobPost.create({
-  ...req.body,
-  startTime: istStartTime,
-  endTime: istEndTime,
-  userId,
-});
-return creation;
-
+  const creation = await JoblessJobPost.create({
+    ...req.body,
+    startTime: istStartTime,
+    endTime: istEndTime,
+    userId,
+  });
+  return creation;
 };
 
 const UpdateJobPost = async (req) => {
@@ -45,7 +44,7 @@ const fetchJobPost = async (req) => {
 const fetchCurrentActiveJobs = async (req) => {
   const currentTime = new Date();
   const validEnd = await JoblessJobPost.find({ endTime: { $gte: currentTime } });
-  console.log(validEnd,"validEnd");
+  console.log(validEnd, 'validEnd');
   return validEnd;
 };
 
