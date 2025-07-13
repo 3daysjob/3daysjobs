f68bbcc0f9196e21ff537167804828c06b8726f2
muthamizhyadav <muthamizhyadav@gmail.com>
2025-07-13 13:06:32 +0530

    Date added in scheduled functionaity

--- src/services/joblessday/jobless.jobpost.service.js
+++ src/services/joblessday/jobless.jobpost.service.js
@@ -130,8 +130,18 @@ const findjobById = async (req) => {
 };
 
 const ApplyJob = async (req) => {
-  const applyJob = await JoblessApplication.create(req.body);
-  return applyJob;
+  const { candidateId, recruiterId, jobId } = req.body;
+  let findExistApplication = await JoblessApplication.findOne({ candidateId, recruiterId, jobId });
+  if (findExistApplication) {
+    findExistApplication.status = req.body.status || findExistApplication.status;
+    findExistApplication.date = req.body.date || '';
+    findExistApplication.time = req.body.time || '';
+    await findExistApplication.save();
+    return findExistApplication;
+  } else {
+    const applyJob = await JoblessApplication.create(req.body);
+    return applyJob;
+  }
 };
 
 const getAppliedCandidatesByRecruiter = async (req) => {
