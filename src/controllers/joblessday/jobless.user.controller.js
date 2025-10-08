78ce00cc474ff2a816e7368dc4804d56448db77e
SelvanSaksh <muthamizh.selvan@sakksh.com>
2025-10-08 23:49:46 +0530

    generate token

--- src/controllers/joblessday/jobless.user.controller.js
+++ src/controllers/joblessday/jobless.user.controller.js
@@ -2,6 +2,7 @@ const httpStatus = require('http-status');
 const ApiError = require('../../utils/ApiError');
 const catchAsync = require('../../utils/catchAsync');
 const JoblessUserServices = require('../../services/joblessday/jobless.user.service');
+const { GeneretaeAuthToken } = require('../../middlewares/genToken');
 
 const createJoblessUser = catchAsync(async (req, res) => {
   const user = await JoblessUserServices.createJoblessUser(req.body);
