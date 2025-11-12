8575310da19774866a296741758784f0d8b9fefa
SelvanSaksh <muthamizh.selvan@sakksh.com>
2025-11-12 19:05:15 +0530

    COmpany details added

--- src/models/joblessday/jobless.user.model.js
+++ src/models/joblessday/jobless.user.model.js
@@ -11,12 +11,10 @@ const JoblessuserSchema = new mongoose.Schema(
     },
     name: {
       type: String,
-      required: true,
       trim: true,
     },
     email: {
       type: String,
-      required: true,
       unique: true,
       trim: true,
       lowercase: true,
